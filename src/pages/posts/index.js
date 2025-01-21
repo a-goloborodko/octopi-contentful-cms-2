a84d7221f7b46f9ca0777e097eb817385f5f002a
Alex Goloborodko <aleksandr.goloborodko@ukad-group.com>
2025-01-21 13:35:57 +0200

    commit changes

--- src/pages/posts/index.js
+++ src/pages/posts/index.js
@@ -1,7 +1,8 @@
 import { client } from '@/lib/contentful/client'
 import PostCard from '@/components/posts/PostCard'
+//import { throwIfDisallowedDynamic } from 'next/dist/server/app-render/dynamic-rendering';
 
-const Postspage = () => {
+const Postspage = ({ posts }) => {
   return (
     // <div>
     //   <h1 className="text-3xl font-bold">Posts Page</h1>
@@ -12,6 +13,10 @@ const Postspage = () => {
           {posts.map((post, i) => (
             <PostCard key={post.fields.slug} post={post} />
           ))}
+
+          {/* {posts.map((post, i) => (
+            <span>{post.fields.title}</span>
+          ))} */}
         </ul>
       </div>
     </section>
